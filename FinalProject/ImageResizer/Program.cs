90751abbd7a76b9046e43d40a644eade0c67b192
Robert Veira <robert.veira@knights.ucf.edu>
2015-12-09 20:01:18 -0500

    CAP 6105-Final Project: Created an ImageResizer application that will resize images in a folder to a particular size.  So far, the folder path and the image size are hard coded.  (C:/food_samples and 256x256 respectfully)

---
+++ FinalProject/ImageResizer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageResizer.Properties;
+using ImageResizer.Utils;
+
+namespace ImageResizer
+{
+    /// <summary>
+    /// Main class created to resize sample images from our food sketch image database.
+    /// </summary>
+    class Program
+    {
+        private const int KSideLength = 256;
+
+        private static void Main(string[] args)
+        {
+            var filePaths = Directory.GetFiles(Settings.Default.FoodSamplePath, "*.png", SearchOption.AllDirectories);
+
+            Console.WriteLine("Resizing {0} files to {1} x {1} px", filePaths.Length, KSideLength);
+
+            for(var i = 0; i < filePaths.Length; ++i)
+            {
+                var bitmap = new Bitmap(filePaths[i]);
+                var resizedBitmap = ImageUtils.ResizeImage(bitmap, new Size(KSideLength, KSideLength));
+
+                bitmap.Dispose();
+
+                resizedBitmap.Save(filePaths[i]);
+                resizedBitmap.Dispose();
+
+                Console.WriteLine("Resized image {0} of {1}", i + 1, filePaths.Length);
+            }
+        }
+    }
+}
---
+++ FinalProject/ImageResizer/Utils/ImageUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+
+namespace ImageResizer.Utils
+{
+    public static class ImageUtils
+    {
+        public static Bitmap ResizeImage(Bitmap imgToResize, Size size)
+        {
+            try
+            {
+                var b = new Bitmap(size.Width, size.Height);
+                using (var g = Graphics.FromImage((Image)b))
+                {
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    g.DrawImage(imgToResize, 0, 0, size.Width, size.Height);
+                }
+                return b;
+            }
+            catch
+            {
+                Console.WriteLine("Bitmap could not be resized");
+                return imgToResize;
+            }
+        }
+    }
+}
